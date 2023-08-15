0e28f186ea4b40b32c35d937797ebbcde0af2bf6
Kae <139962400+kaedeluxe@users.noreply.github.com>
2023-08-15 15:53:10 +0100

    js hover effect implement
    hover effect on mouse over now complete
    fades out after 5s with css transition timer

--- script.js
+++ script.js
@@ -1,22 +1,54 @@
 const createEl = (userEl) => document.createElement(userEl),
   query = (userQ) => document.querySelector(userQ),
+  queryAll = (userQ) => document.querySelectorAll(userQ),
   container = query('#main-container'),
-  createBtn = query('#create-btn');
+  createBtn = query('#create-btn'),
+  inputNum = query('#input-num');
 
-let squaresNum = 16;
+let squareNum, squareEl, squareAll;
 
 function createSquares() {
-  for (let i = 0; i < (squaresNum * squaresNum); i++) {
-    let square = createEl('div'),
-      squarePct = 100 / squaresNum;
-      // containPct = squarePct * squaresNum;
-    square.className = 'square';
-    square.style.flex = `0 0 ${squarePct}%`;
-    square.style.height = `${squarePct}%`;
-    square.style.width = `${squarePct}%`;
-    // container.style.width = `${containPct}%`;
-    container.appendChild(square);
+  if (inputNum.value >= 2 && inputNum.value <= 100) {
+    squareNum = inputNum.value;
+  } else {
+    alert('Invalid range: please enter a number between 2 and 100');
+    return;
   }
-};
+  while (container.hasChildNodes()) {
+    container.removeChild(container.lastChild);
+  }
+  for (let i = 0; i < squareNum * squareNum; i++) {
+    let squarePct = 100 / squareNum;
+    squareEl = createEl('div');
+    squareEl.className = 'square';
+    squareEl.style.flex = `0 0 ${squarePct}%`;
+    squareEl.style.height = `${squarePct}%`;
+    squareEl.style.width = `${squarePct}%`;
+    container.appendChild(squareEl);
+  }
+}
 
-createSquares();
+function hoverIn(currSquare) {
+  currSquare.classList.add('square-hover');
+}
+
+function hoverOut(currSquare) {
+  currSquare.classList.remove('square-hover');
+}
+
+function addEvent() {
+  squareAll.forEach((square) => {
+    square.addEventListener('mouseenter', () => {
+      hoverIn(square);
+    });
+    square.addEventListener('mouseleave', () => {
+      hoverOut(square);
+    });
+  });
+}
+
+createBtn.addEventListener('click', () => {
+  createSquares();
+  squareAll = queryAll('.square');
+  addEvent();
+});
